ea8e71b19f63f6bb393fa66100a9d75f35148761
Evan Tsai <evan@err7.net>
2020-06-18 19:29:36 +0800

    SB-1282 Use fonts and colors from Streambank

--- app/Config/setting-defaults.php
+++ app/Config/setting-defaults.php
@@ -10,17 +10,17 @@
 
 return [
 
-    'app-name'             => 'BookStack',
+    'app-name'             => 'Library',
     'app-logo'             => '',
     'app-name-header'      => true,
     'app-editor'           => 'wysiwyg',
-    'app-color'            => '#206ea7',
+    'app-color'            => '#214E72',
     'app-color-light'      => 'rgba(32,110,167,0.15)',
-    'bookshelf-color'      => '#a94747',
-    'book-color'           => '#077b70',
-    'chapter-color'        => '#af4d0d',
-    'page-color'           => '#206ea7',
-    'page-draft-color'     => '#7e50b1',
+    'bookshelf-color'      => '#214E72',
+    'book-color'           => '#214E72',
+    'chapter-color'        => '#214E72',
+    'page-color'           => '#214E72',
+    'page-draft-color'     => '#214E72',
     'app-custom-head'      => false,
     'registration-enabled' => false,
 
